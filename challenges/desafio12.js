a948b1f37a9970bee1ff4704a57131377d24e549
mcamillosf <matheuscamillo@hotmail.com.br>
2022-04-13 21:54:24 -0300

    feat. resolvido desafio 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,1 +1,3 @@
-// db.produtos.updateMany();
+db.produtos.updateMany({ ingredientes: { $nin: ["ketchup"] }, nome: { $ne: "McChicken" } }, { $push: { ingredientes: "ketchup" } });
+
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
